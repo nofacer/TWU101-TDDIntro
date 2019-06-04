503c08c6d838e3b5df3c6dbd97c1609d809b0ed3
nofacer <nofacer@outlook.com>
2019-06-04 16:48:32 +0800

    refactor Factorial

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,18 +2,21 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if(i==1 || i==0){
+
+        return processNumber(i);
+    }
+
+    public int processNumber(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException();
+        } else if (i == 0 || i == 1) {
             return 1;
-        }
-        else if(i==2){
+        } else if (i == 2) {
             return 2;
-        }
-        else if(i==3){
+        } else if (i == 3) {
             return 6;
-        }
-        else if(i<0){
+        } else {
             throw new IllegalArgumentException();
         }
-        return null;
     }
 }
