d3bd6042c99373659fc55904a4f8c9a4080f48c8
nofacer <nofacer@outlook.com>
2019-06-04 16:37:13 +0800

    shouldThrowIllegalArgumentExceptionWhenNumberIsNegative

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -11,6 +11,9 @@ public class Factorial {
         else if(i==3){
             return 6;
         }
+        else if(i<0){
+            throw new IllegalArgumentException();
+        }
         return null;
     }
 }
