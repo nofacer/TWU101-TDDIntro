b069e13ab9dafc352fd226fa39dc7ab6459b561b
nofacer <nofacer@outlook.com>
2019-06-04 16:29:33 +0800

    shouldReturnOneWhenNumberIsZero

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,7 +2,7 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if(i==1){
+        if(i==1 || i==0){
             return 1;
         }
         else if(i==2){
