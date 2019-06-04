fe121dc5fdb752ff8a34d8e86db3bcf6e7ec8b7c
nofacer <nofacer@outlook.com>
2019-06-04 17:00:51 +0800

    init Account.JAVA

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,9 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int balance ;
+
+    public Account(int initBalance) {
+        balance = initBalance;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -1,23 +1,28 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class AccountTests {
-    @Test
-    @Ignore  // Remove each @Ignore and implement test
-    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+    private  Account account ;
+
+    @Before
+    public void init(){
+        account=new Account(5);
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+
+    }
+
+    @Test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
 
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
 
     }
