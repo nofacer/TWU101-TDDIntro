c590bbbaa8397a338dfd45efeddc92ff20a4ba26
nofacer <nofacer@outlook.com>
2019-06-04 17:05:45 +0800

    shouldIncreaseMyBalanceWhenIDepositMoney

--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -4,17 +4,21 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
     private  Account account ;
 
     @Before
     public void init(){
-        account=new Account(5);
+        account=new Account(100);
     }
 
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-
+        account.deposite(50);
+        assertThat(account.getBalance(),is(150));
     }
 
     @Test
