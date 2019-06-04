ba2b326bdb4b5d9ce7bfcaca82f3fbc2e3048b53
nofacer <nofacer@outlook.com>
2019-06-04 16:28:02 +0800

    shouldReturnOneWhenNumberIsOne

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,11 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        if(i==1){
+            return 1;
+        }
+        else{
+            return null;
+        }
     }
 }
